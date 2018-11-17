a4cd773e5c31af608300a0c8f4a094d2affd6515
Jerry Deng <jingchengdeng@hotmail.com>
2018-11-17 13:52:38 -0600

    update video frame capture function

--- smoke.py
+++ smoke.py
@@ -9,15 +9,30 @@ import cv2
 import numpy as np
 import math
 import os
+import sys
 
 def main():
-    print("main")
+    try:
+        video_src = sys.argv[1]
+    except IndexError:
+        print('Video Pass Error')
+    cap = cv2.VideoCapture(video_src)
+    frame_count = 1
+    while True:
+        ret, frame = cap.read()
+        if frame is None:
+            print("Video reach end.")
+            break
+        # extract frames for every X frame
 
+        if frame_count % 10 == 0:
+            cv2.imwrite("test/frame%d.jpg" % frame_count, frame)
 
+        # Press Key Q to exit
+        if (cv2.waitKey(10) & 0xFF) == 'Q':
+            break
 
-
-
-
+        frame_count += 1
 
 
 if __name__ == "__main__":
