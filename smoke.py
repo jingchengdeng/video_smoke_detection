b93cf9359bc72e8d0ccad4bc863e73d9f79ca659
fengsu91 <sfeng14@hawk.iit.edu>
2018-11-17 13:10:25 -0600

    d

---
+++ smoke.py
@@ -0,0 +1,24 @@
+'''
+CS512 Project
+Smoke detection
+Su Feng
+Jingcheng Deng
+'''
+
+import cv2
+import numpy as np
+import math
+import os
+
+def main():
+    print("main")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
