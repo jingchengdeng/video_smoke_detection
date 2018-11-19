1591b472683fa11fc947c92a7fd80f5f4f7d1776
Jerry Deng <jingchengdeng@hotmail.com>
2018-11-18 20:16:27 -0600

    simplify program

--- smoke.py
+++ smoke.py
@@ -164,7 +164,7 @@ def transmission(img, A, blocksize, ori):
 def main():
     print("main")
     # motionloop()
-    image = cv2.imread("data/smk1.jpg")
+    image = cv2.imread("test/frame10.jpg")
     image = resizeimge(image, 500)
     I = image.astype('float64') / 255
     darkChannel = getDarkChannel(I, 15)
@@ -187,30 +187,30 @@ def main():
 
     # print(t)
 
-'''
-    try:
-        video_src = sys.argv[1]
-    except IndexError:
-        print('Video Pass Error')
-    cap = cv2.VideoCapture(video_src)
-    frame_count = 1
-    while True:
-        ret, frame = cap.read()
-        if frame is None:
-            print("Video reach end.")
-            break
-        # extract frames for every X frame
 
-        if frame_count % 10 == 0:
-            cv2.imwrite("test/frame%d.jpg" % frame_count, frame)
-
-        # Press Key Q to exit
-        if (cv2.waitKey(10) & 0xFF) == 'Q':
-            break
-
-        frame_count += 1
-'''
-
+#     try:
+#         video_src = sys.argv[1]
+#     except IndexError:
+#         print('Video Pass Error')
+#     cap = cv2.VideoCapture(video_src)
+#     frame_count = 1
+#     while True:
+#         ret, frame = cap.read()
+#         if frame is None:
+#             print("Video reach end.")
+#             break
+#         # extract frames for every X frame
+#
+#         if frame_count % 5 == 0:
+#             cv2.imwrite("test/frame%d.jpg" % frame_count, frame)
+#
+#         # Press Key Q to exit
+#         if (cv2.waitKey(10) & 0xFF) == 'Q':
+#             break
+#
+#         frame_count += 1
+#
+#
 
 if __name__ == "__main__":
     main()
